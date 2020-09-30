481a470d08ffc66d97f248f387361c415eee96ba
Batman <bgmyhan@gmail.com>
2020-09-30 16:59:59 -0700

    created redux store and included chrome devtools installation

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+
 
 import App from './components/App'
+import reducers from './reducers'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+
+
+ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+//  Chrome Redux devtools installed. To utitlize type in the following path after the url. ?debug_session=someString //
+const store = createStore(reducers, composeEnhancers(applyMiddleware()))
+
+ReactDOM.render(
+
+<Provider store={store}>
+    <App />
+</Provider>
+,
+ document.querySelector('#root'))
+
