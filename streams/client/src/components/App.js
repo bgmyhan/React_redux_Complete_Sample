456fa81003b2e46dcce9267c71cd6bc6629085d0
Batman <bgmyhan@gmail.com>
2020-10-11 17:41:13 -0700

    basic Crud Operations in React

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate'
 import StreamDelete from './streams/StreamDelete'
 import StreamEdit from './streams/StreamEdit'
 import StreamShow from './streams/StreamShow'
 import StreamList from './streams/StreamLIst'
 import Header from './header'
+import history from '../history'
 
 const App = () => {
     return (
         <div className="ui container">
       
-            <BrowserRouter>
+            <Router history={history}>
             <Header />
+            <Switch>
                 <Route path='/' exact component={StreamList} />
                 <Route path='/streams/new' exact component={StreamCreate} />
-                <Route path='/streams/edit' exact component={StreamEdit} />
-                <Route path='/streams/delete' exact component={StreamDelete} />
-                <Route path='/streams/show' exact component={StreamShow} />
-            </BrowserRouter>
+                <Route path='/streams/edit/:id' exact component={StreamEdit} />
+                <Route path='/streams/delete/:id' exact component={StreamDelete} />
+                <Route path='/streams/:id' exact component={StreamShow} />
+            </Switch>
+            </Router>
         </div>
     )
 };
--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
+import reduxThunk from 'redux-thunk'
 
 
 import App from './components/App'
@@ -13,7 +14,7 @@ import reducers from './reducers'
 
 
 //  Chrome Redux devtools installed. To utitlize type in the following path after the url. ?debug_session=someString //
-const store = createStore(reducers, composeEnhancers(applyMiddleware()))
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)))
 
 ReactDOM.render(
 
